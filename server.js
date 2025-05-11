cc400350db64b9c01670db28f8b499171f92aec0
HuyCao2009 <huydeptrainhungkhongpd@gmail.com>
2025-05-11 21:45:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -36,11 +36,18 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Avatar upload
 const storage = multer.diskStorage({
-  destination: './uploads/',
+  destination: (req, file, cb) => {
+    const uploadDir = './uploads/';
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   }
 });
+
 const upload = multer({ storage });
 
 // Routes
