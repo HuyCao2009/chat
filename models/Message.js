fbc95a5a9d221cb22249eed625611081b2323345
HuyCao2009 <huydeptrainhungkhongpd@gmail.com>
2025-05-11 19:35:25 +0700

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,7 +1,10 @@
+const mongoose = require('mongoose');
+
 const MessageSchema = new mongoose.Schema({
   from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   to: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   text: String,
   time: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Message', MessageSchema);
