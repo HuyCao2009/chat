bdfae71bb224208d943c08d8237ae52e5bc1098e
HuyCao2009 <huydeptrainhungkhongpd@gmail.com>
2025-05-11 19:25:53 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: String,
   password: String,
+  avatar: String,
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
